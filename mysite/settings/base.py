81b3fd05a7e9730f8e1580d456440d54bb505774
RAHUL9545416 <rp9545416@gmail.com>
2020-08-13 19:58:17 +0530

    them

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -25,7 +25,9 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 
 INSTALLED_APPS = [
     'home',
-    'search',
+    'flex',
+    'search',    
+   # 'streams',
 
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
